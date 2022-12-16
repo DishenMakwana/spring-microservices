277498f629cbd8d3c3d45806cd4ebbcbfa5e57d5
DISHEN <dishenmakwana.dm@gmail.com>
2022-12-16 22:00:30 +0530

    ADD: code

---
+++ 01.basic-demo/src/main/java/com/basic/springboot/basicdemo/BasicDemoApplication.java
@@ -0,0 +1,13 @@
+package com.basic.springboot.basicdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicDemoApplication.class, args);
+	}
+
+}
---
+++ 01.basic-demo/src/test/java/com/basic/springboot/basicdemo/BasicDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.springboot.basicdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
